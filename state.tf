8af074b1afa6c310eec8badcdd96b03553abd809
Ubuntu <ubuntu@ip-172-31-85-87.ec2.internal>
2020-04-09 00:23:02 +0000

    change s3 repo name

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-34scj-333652"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
